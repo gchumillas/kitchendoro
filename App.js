975296d61b4cfc17ddd240a8dd23358ac25d2fee
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-10 22:16:45 +0100

    eslint: fixes

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+const App = () => {
   return <View style={styles.container}>
     <Text>Open up App.js to start working on your app!</Text>
     <StatusBar style="auto" />
@@ -14,6 +14,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 })
+
+export default App
