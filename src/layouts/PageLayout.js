dfefdcdecc3d4848d85c4d8827cb509fe5a5bbfe
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-11 11:24:24 +0100

    css: add marginTop

--- src/layouts/PageLayout.js
+++ src/layouts/PageLayout.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View } from 'react-native'
+import { StatusBar as SB, View } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { useTailwind } from 'tailwind-rn'
 
 const PageLayout = ({ children }) => {
   const tw = useTailwind()
 
-  return <View style={tw('flex h-full bg-white items-center justify-center')}>
+  return <View style={{ ...tw('flex h-full bg-white items-center justify-center'), marginTop: SB.currentHeight }}>
     {children}
     <StatusBar style="auto" />
   </View>
