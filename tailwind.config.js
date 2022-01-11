ba52304f540e769061ead434bf4c825e328408f3
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-11 10:43:25 +0100

    extends tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': colors.gray[800],
+      },
+    },
   },
   plugins: [],
 }
