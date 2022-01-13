4bbf71c47c67b62de3eb5dc5f97a980e4a3b109d
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-13 16:30:08 +0100

    add utils/ModalDialog

---
+++ src/components/utils/ModalDialog.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Modal, Pressable, StyleSheet, View, StatusBar } from 'react-native'
+import { tw } from '~/src/libs/tailwind'
+
+const Component = ({ visible = false, onRequestClose = undefined, children }) => {
+  return <Modal animationType="fade" transparent visible={visible} onRequestClose={onRequestClose} statusBarTranslucent>
+    <Pressable onPress={onRequestClose}>
+      <View style={styles.wrapper}>
+        <View onStartShouldSetResponder={_ => true} style={styles.box}>
+          {children}
+        </View>
+      </View>
+    </Pressable>
+  </Modal>
+}
+
+const styles = StyleSheet.create({
+  wrapper: tw('flex h-full justify-center items-center bg-black bg-opacity-30 pt-4'),
+  box: {
+    ...tw('bg-white p-5 rounded-md w-11/12'),
+    marginTop: StatusBar.currentHeight
+  }
+})
+
+export default Component
