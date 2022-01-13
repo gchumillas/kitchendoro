c9bac152efc5f59db105f2c9f9082a7a2c78f9f0
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-13 10:48:46 +0100

    update menu icon

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,6 +7,7 @@ import ProgressBar from '~/src/components/display/ProgressBar'
 
 const Timer = ({ style = undefined }) => {
   const value = React.useMemo(_ => Math.random(), [])
+  const iconSize = 35
 
   return <View style={{ ...style, ...tw('border-2 rounded-md border-white px-2') }}>
     <View style={tw('h-6 flex justify-center items-center')}>
@@ -14,13 +15,13 @@ const Timer = ({ style = undefined }) => {
     </View>
     <View style={tw('flex flex-row items-center justify-between')}>
       <Pressable>
-        <MenuIcon fill="white" />
+        <MenuIcon fill="white" width={iconSize} height={iconSize} />
       </Pressable>
       <Text style={{ ...tw('text-4xl text-white mt-1'), fontFamily: 'RobotoMono_400Regular' }}>
         15:45:00
       </Text>
       <Pressable>
-        <PlayIcon fill="white" />
+        <PlayIcon fill="white" width={iconSize} height={iconSize} />
       </Pressable>
     </View>
     <View style={tw('h-6 flex justify-center')}>
