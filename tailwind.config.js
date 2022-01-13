38ca21c6b1407de96c4f60fe16261d6dd92c2303
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-13 10:56:49 +0100

    tailwind: update config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': colors.gray[800],
+        'light': colors.white,
+        'dark': colors.black
       },
     },
   },
