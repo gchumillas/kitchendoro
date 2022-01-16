9afcae6aa6aa45c925771476e998c76099f9c746
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-16 20:43:17 +0100

    prevent from exceding max time

--- src/pages/HomePage/Timer.js
+++ src/pages/HomePage/Timer.js
@@ -8,6 +8,8 @@ import StopIcon from '~/assets/icons/stop.svg'
 import { ProgressBar, Text } from '~/src/components/display'
 import { IconButton } from '~/src/components/inputs'
 
+const maxSeconds = 99 * 3600 + 59 * 60 + 60
+
 const dd = val => {
   const str = `${val}`
   return `${'0'.repeat(Math.max(0, 2 - str.length))}${str}`
@@ -22,10 +24,10 @@ const Timer = ({ seconds, name, style = undefined }) => {
   const overTime = running && countdown <= 0
 
   const time = React.useMemo(_ => {
-    const val = Math.abs(countdown)
+    const val = Math.abs(countdown) % maxSeconds
     const ss = val % 60
     const mm = Math.floor(val / 60) % 60
-    const hh = Math.floor(val / 3600)
+    const hh = Math.floor(val / 3600) % 100
 
     return `${dd(hh)}:${dd(mm)}:${dd(ss)}`
   }, [countdown])
