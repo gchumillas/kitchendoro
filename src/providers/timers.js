12badb890512407a09de3c130671b4fd1d59259b
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-16 17:00:05 +0100

    providers: add timers

---
+++ src/providers/timers.js
@@ -0,0 +1,44 @@
+import * as ss from 'expo-secure-store'
+import uuid from 'react-native-uuid'
+import { fix, pipes } from '@gchumillas/schema-fixer'
+
+const fixTimers = timers => fix(timers, pipes.array({ type: { id: 'string', name: 'string', number: 'number' } }))
+
+const saveTimers = async timers => {
+  await ss.setItemAsync('timers', JSON.stringify(fixTimers(timers)))
+}
+
+/**
+ * @returns {Promise<{ id: string, name: string, seconds: number }[]>}
+ */
+export const getTimers = async _ => {
+  try {
+    return fixTimers(JSON.parse(await ss.getItemAsync('timers')))
+  } catch (err) {
+    console.error(err)
+    return []
+  }
+}
+
+/**
+ * @param {object} props
+ * @param {string} props.name
+ * @param {number} props.seconds
+ * @returns {Promise<string>}
+ */
+export const createTimer = async ({ name, seconds }) => {
+  const timers = await getTimers()
+  const id = uuid.v1()
+
+  await saveTimers([{ id, name, seconds }, ...timers])
+  return id
+}
+
+/**
+ * @param {string} id
+ */
+export const deleteTimer = async id => {
+  const timers = await getTimers()
+
+  await saveTimers(timers.filter(x => x.id != id))
+}
