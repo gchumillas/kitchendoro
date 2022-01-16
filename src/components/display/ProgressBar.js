487aa89923de9bdef3fa9b89725797aca25fc9a4
Gonzalo Chumillas <gchumillas@users.noreply.github.com>
2022-01-16 23:04:24 +0100

    Update ProgressBar.js

--- src/components/display/ProgressBar.js
+++ src/components/display/ProgressBar.js
@@ -2,13 +2,9 @@ import React from 'react'
 import { View } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 
-/**
- *
- * @param {object} params
- * @param {number} params.value A value between 0 and 1
- * @returns
- */
-const ProgressBar = ({ value }) => {
+const ProgressBar = ({
+  value // a value between 0 and 1
+}) => {
   return <View style={tw('h-2')}>
     <View style={{ ...tw('h-2 bg-light'), width: `${100 * value}%` }} />
   </View>
