74274b3c641e27848431b817b67fcbec9805f0e8
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-16 17:28:25 +0100

    refactor

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -5,9 +5,22 @@ import MenuIcon from '~/assets/icons/menu.svg'
 import PlayIcon from '~/assets/icons/play.svg'
 import { ProgressBar, Icon, Text } from '~/src/components/display'
 
-const Timer = ({ style = undefined }) => {
+const dd = val => {
+  const str = `${val}`
+  return `${'0'.repeat(Math.max(0, 2 - str.length))}${str}`
+}
+
+const Timer = ({ seconds, style = undefined }) => {
   const value = React.useMemo(_ => Math.random(), [])
   const iconSize = 35
+
+  const time = React.useMemo(_ => {
+    const ss = seconds % 60
+    const mm = Math.floor(seconds / 60) % 60
+    const hh = Math.floor(seconds / 3600)
+
+    return `${dd(hh)}:${dd(mm)}:${dd(ss)}`
+  }, [seconds])
 
   return <View style={[tw('border-2 rounded-md border-light px-2'), style]}>
     <View style={tw('h-6 flex justify-center items-center')}>
@@ -18,7 +31,7 @@ const Timer = ({ style = undefined }) => {
         <Icon component={MenuIcon} size={iconSize} />
       </Pressable>
       <Text style={{ ...tw('text-4xl mt-1'), fontFamily: 'RobotoMono_400Regular' }}>
-        15:45:00
+        {time}
       </Text>
       <Pressable>
         <Icon component={PlayIcon} size={iconSize} />
