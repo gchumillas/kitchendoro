3c05358066241ee75296f65b9ad48f72e63c67b7
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-18 22:43:35 +0100

    ProgressBar: set bg color

--- src/components/display/ProgressBar.js
+++ src/components/display/ProgressBar.js
@@ -6,7 +6,8 @@ const ProgressBar = ({
   value, // a value between 0 and 1
   color = '#ffffff'
 }) => {
-  return <View style={tw('h-2')}>
+  return <View style={tw('h-2 relative')}>
+    <View style={{ ...tw('h-2 absolute top-0 left-0 w-full'), backgroundColor: color, opacity: 0.2 }} />
     <View style={{ ...tw('h-2'), width: `${100 * value}%`, backgroundColor: color }} />
   </View>
 }
