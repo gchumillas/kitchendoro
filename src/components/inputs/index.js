e769e6846cc6ab57e726fd78d8dfefd2e53094df
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-23 20:00:53 +0100

    remove inputs/index.js

--- src/components/inputs/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TimerInput } from './TimerInput'
-export { default as Button } from './Button'
-export { default as IconButton } from './IconButton'
-export { default as TextField } from './TextField'
