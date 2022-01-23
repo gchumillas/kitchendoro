ddf4a0444376f7dec1b7ce503cda3f84ad792212
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-23 19:59:09 +0100

    remove display/index.js

--- src/components/display/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Icon } from './Icon'
-export { default as ProgressBar } from './ProgressBar'
-export { default as Text } from './Text'
-export { default as Timer } from './Timer'
