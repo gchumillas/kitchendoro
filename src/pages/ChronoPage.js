973a774dd1eddc10062cd82b49e241f7049ec3de
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-24 20:47:58 +0100

    refactor

--- src/pages/ChronoPage.js
+++ src/pages/ChronoPage.js
@@ -3,6 +3,7 @@ import { View } from 'react-native'
 import { tw } from '~/src/libs/tailwind'
 import { getChrono, startChrono, resetChrono } from '~/src/providers/chrono'
 import { useChrono } from '~/src/hooks/timer'
+import { parseSeconds } from '~/src/libs/time'
 import Text from '~/src/components/display/Text'
 import IconButton from '~/src/components/inputs/IconButton'
 import Footer from '~/src/components/app/Footer'
@@ -12,16 +13,10 @@ import PlayIcon from '~/assets/icons/play.svg'
 
 const iconSize = 55
 
-const maxSeconds = 99 * 3600 + 59 * 60 + 60
-
-const dd = val => {
-  const str = `${val}`
-  return `${'0'.repeat(Math.max(0, 2 - str.length))}${str}`
-}
-
 const ChronoPage = _ => {
   const [chrono, setChrono] = React.useState({ startFrom: 0, running: false })
   const seconds = useChrono(chrono)
+  const time = React.useMemo(_ => parseSeconds(seconds), [seconds])
   const reload = async _ => setChrono(await getChrono())
 
   const doStartChrono = async _ => {
@@ -33,16 +28,6 @@ const ChronoPage = _ => {
     await resetChrono()
     reload()
   }
-
-  // TODO: don't repeat yourself
-  const time = React.useMemo(_ => {
-    const val = Math.abs(seconds) % maxSeconds
-    const ss = val % 60
-    const mm = Math.floor(val / 60) % 60
-    const hh = Math.floor(val / 3600) % 100
-
-    return `${dd(hh)}:${dd(mm)}:${dd(ss)}`
-  }, [seconds])
 
   return <PageLayout footer={<Footer />}>
     <View style={tw('flex h-full items-center justify-center')}>
