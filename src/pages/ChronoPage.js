f5d83fb13c172e91f824f7c9f2350224c6ff953c
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-24 18:31:33 +0100

    ChronoPage: styling

--- src/pages/ChronoPage.js
+++ src/pages/ChronoPage.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { View } from 'react-native'
+import { tw } from '~/src/libs/tailwind'
 import Text from '~/src/components/display/Text'
+import IconButton from '~/src/components/inputs/IconButton'
 import Footer from '~/src/components/app/Footer'
 import PageLayout from '~/src/layouts/PageLayout'
+import ResetIcon from '~/assets/icons/reset.svg'
+import PlayIcon from '~/assets/icons/play.svg'
 
 const ChronoPage = _ => {
+  const iconSize = 55
+
   return <PageLayout footer={<Footer />}>
-    <Text>chrono page</Text>
+    <View style={tw('flex h-full items-center justify-center')}>
+      <View style={tw('')}>
+        <Text style={{ ...tw('mb-7 text-center text-6xl'), fontFamily: 'RobotoMono_400Regular' }}>
+          00:00:00
+        </Text>
+        <View style={tw('flex flex-row justify-evenly items-center')}>
+          <IconButton icon={ResetIcon} size={iconSize} />
+          <IconButton icon={PlayIcon} size={iconSize} />
+        </View>
+      </View>
+    </View>
   </PageLayout>
 }
 
