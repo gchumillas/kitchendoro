c413ec57e2f5c76273e1d5bf7f24b993703b4153
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2022-01-25 17:12:24 +0100

    bugfix

--- src/hooks/timer.js
+++ src/hooks/timer.js
@@ -2,15 +2,14 @@ import React from 'react'
 
 export const useCountdown = ({ running, startFrom, seconds }) => {
   const [countdown, setCountdown] = React.useState(0)
+  const elapsedTime = ({ running, seconds, startFrom }) => {
+    return running ? seconds - Math.floor((Date.now() - startFrom) / 1000) : seconds
+  }
 
   React.useEffect(_ => {
-    setCountdown(seconds)
-
+    setCountdown(elapsedTime({ running, seconds, startFrom }))
     const interval = setInterval(_ => {
-      if (running) {
-        const elapsedTime = Math.floor((Date.now() - startFrom) / 1000)
-        setCountdown(seconds - elapsedTime)
-      }
+      setCountdown(elapsedTime({ running, seconds, startFrom }))
     }, 1000)
 
     return _ => {
